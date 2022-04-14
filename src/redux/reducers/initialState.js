715fe3809f6523c6a126e439a83635b02157d1ec
Kaltyga Maxim <kaltyga.exceedteam@gmail.com>
2022-04-14 17:21:02 +0300

    Started add something new

--- src/redux/reducers/initialState.js
+++ src/redux/reducers/initialState.js
@@ -1,7 +1,9 @@
+import {gameMap} from "../../utils/gameMap"
+
 let hs = localStorage.getItem('highscore') ? localStorage.getItem('highscore') : 0;
 
 export const initialState = {
-    gameMap : 1,
+    gameMap : gameMap,
     score: 0,
     highscore: hs
 }

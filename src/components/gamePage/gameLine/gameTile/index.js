bf0ec02a9f50dec272fe6a8f9a5a2970971a7621
Kaltyga Maxim <kaltyga.exceedteam@gmail.com>
2022-04-15 12:31:50 +0300

    Update game map

--- src/components/gamePage/gameLine/gameTile/index.js
+++ src/components/gamePage/gameLine/gameTile/index.js
@@ -3,9 +3,13 @@ import style from './gameTile.module.css'
 
 
 const GameTile = ({type}) => {
-    let selectedDiv = type === 0 ? <div className={style.space}/> : type === 1 ?
-        <div className={style.wall}/> : type === 2 ? <div className={style.space}/> : type === 3 ?
-            <div className={style.gateSpace}><div className={style.gate}></div></div> : <div className={style.space}/>
+    let selectedDiv = type === 0 ?
+        <div className={style.space}>{type === 0 && <div className={style.dot}/>}</div> : type === 1 ?
+            <div className={style.wall}/> : type === 2 ?
+                <div className={style.space}>{type === 2 && <div className={style.bigDot}/>}</div> : type === 3 ?
+                    <div className={style.enemySpace}>
+                        <div className={style.gate}/>
+                    </div> : type === 4 ? <div className={style.space}/> : <div className={style.space}/>
     return (
         selectedDiv
     );
